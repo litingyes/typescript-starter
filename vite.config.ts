e7125c2b7c237eec8f2904491a123f25773cb7a1
litingyes <luz.liting@gmail.com>
2025-02-20 22:44:58 +0800

    feat: replace unbuild with vite

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import external from 'unplugin-external/vite'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      formats: ['es'],
+      fileName: 'index',
+    },
+  },
+  plugins: [
+    dts(),
+    external(),
+  ],
+})
